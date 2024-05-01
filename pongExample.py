1552360ccd5d486c60939ca195405f13531e7b03
theAlienBirb <141907179+nonAlienBirb@users.noreply.github.com>
2024-05-01 01:08:53 -0400

    Update pongExample.py

--- pongExample.py
+++ pongExample.py
@@ -1,21 +1,24 @@
 import gameenvai
 
-
 myGame = gameenvai.Pong()
+
 p1 = gameenvai.Player(1)
+p3 = gameenvai.Player(3)
 p2 = gameenvai.Player(2)
+
 ball = gameenvai.Ball(1)
 ball2 = gameenvai.Ball(2)
+ball3 = gameenvai.Ball(3)
+
 while myGame.isRunning:
-    myGame.updateScreen() 
+    myGame.updateScreen()
     myGame.check()
-    p1.Play('Human')
+    p1.Play()
     p2.Play('Unbeatable')
+    p3.Play('Unbeatable')
     ball.start()
     ball2.start()
-    myGame.draw(ball2.X,ball2.Y,ball2.size,ball2.size)
-    myGame.draw(ball.X,ball.Y,ball.size,ball.size)
-    myGame.draw(p1.X,p1.Y,p1.Width,p1.Height)
-    myGame.draw(p2.X,p2.Y,p2.Width,p2.Height)
+    ball3.start()
+    
     myGame.update()
 myGame.end()
